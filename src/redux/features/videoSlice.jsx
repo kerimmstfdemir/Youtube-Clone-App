4f6ff65852588a5d753af664a530196edad19e95
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-02-18 21:27:32 +0300

    added new state for videoSlice

--- src/redux/features/videoSlice.jsx
+++ src/redux/features/videoSlice.jsx
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialStates = {
+    currentQuery:"",
     searchInput: "",
     videos: {}
 }
@@ -9,6 +10,9 @@ const videoSlice = createSlice({
     name:"videoSlice",
     initialState: initialStates,
     reducers : {
+        currentQueryValue:(state, action) => {
+            state.currentQuery = action.payload
+        },
         searchInputValue:(state, action) => {
             state.searchInput = action.payload
         },
@@ -18,5 +22,5 @@ const videoSlice = createSlice({
     }
 })
 
-export const { searchInputValue, getVideos } = videoSlice.actions
+export const { currentQueryValue, searchInputValue, getVideos } = videoSlice.actions
 export default videoSlice.reducer
